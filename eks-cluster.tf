397d03b09c49167d035576169f8e06d1b2ba0d9d
kodekolli <79526430+kodekolli@users.noreply.github.com>
2021-03-23 14:37:03 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,12 +1,5 @@
-#
-# EKS Cluster Resources
-#  * IAM Role to allow EKS service to manage other AWS services
-#  * EC2 Security Group to allow networking traffic with EKS cluster
-#  * EKS Cluster
-#
-
 resource "aws_iam_role" "demo-cluster" {
-  name = "terraform-eks-demo-cluster"
+  name = "${var.env}-terraform-eks-demo-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -35,7 +28,7 @@ resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSVPCResourceCont
 }
 
 resource "aws_security_group" "demo-cluster" {
-  name        = "terraform-eks-demo-cluster"
+  name        = "${var.env}-terraform-eks-demo-cluster"
   description = "Cluster communication with worker nodes"
   vpc_id      = aws_vpc.demo.id
 
@@ -51,19 +44,10 @@ resource "aws_security_group" "demo-cluster" {
   }
 }
 
-resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
-  cidr_blocks       = [local.workstation-external-cidr]
-  description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.demo-cluster.id
-  to_port           = 443
-  type              = "ingress"
-}
-
 resource "aws_eks_cluster" "demo" {
-  name     = var.cluster-name
+  name     = "${var.env}-${var.cluster-name}"
   role_arn = aws_iam_role.demo-cluster.arn
+  version = 1.19
 
   vpc_config {
     security_group_ids = [aws_security_group.demo-cluster.id]
