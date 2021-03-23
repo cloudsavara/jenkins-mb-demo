1bfe2ac652651a346576a3a04a4fcf0592907589
kodekolli <79526430+kodekolli@users.noreply.github.com>
2021-03-23 14:36:22 +0530

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -1,11 +1,5 @@
-#
-# EKS Worker Nodes Resources
-#  * IAM role allowing Kubernetes actions to access other AWS services
-#  * EKS Node Group to launch worker nodes
-#
-
 resource "aws_iam_role" "demo-node" {
-  name = "terraform-eks-demo-node"
+  name = "${var.env}-terraform-eks-demo-node"
 
   assume_role_policy = <<POLICY
 {
@@ -40,7 +34,7 @@ resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryR
 
 resource "aws_eks_node_group" "demo" {
   cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "demo"
+  node_group_name = "${var.env}-demo"
   instance_types = ["t3.medium"]
   node_role_arn   = aws_iam_role.demo-node.arn
   subnet_ids      = aws_subnet.demo[*].id
